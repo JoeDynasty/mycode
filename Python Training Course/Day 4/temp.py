4b57b0033d77419c2bb92082721834065a13af75
JoeDynasty <123601085+JoeDynasty@users.noreply.github.com>
2023-01-25 15:06:54 -0800

    Add files via upload

---
+++ Python Training Course/Day 4/temp.py
@@ -0,0 +1,12 @@
+"""
+This is a module for temperture conversion
+
+"""
+
+def to_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5/9
+    return celsius
+ 
+def to_fahrenheit(celsius):
+    fahrenheit = celsius * 9/5 + 32
+    return fahrenheit
